6213837e7fc5a243b3d621431a2a9360eec5db5c
Santhosh <ssundar3@sapient.com>
2017-03-10 11:25:03 +0530

    clean up

--- src/molecules/Card.jsx
+++ src/molecules/Card.jsx
@@ -7,21 +7,18 @@ import styles from './Card.css'
 
  const Card = (props) => {
   return (
- <div className={styles.cardContainer}>
-{props.cardData.map((card) =>
-    <div className={styles.card}>
-            <img src={card.image} />
-              <h2 children={card.heading} />
-              <Rating value={4} />
-            <p>{card.text} </p>
-            <Button> Add to Cart</Button>
-         
+    <div className={styles.cardContainer}>
+      {props.cardData.map((card) =>
+        <div className={styles.card}>
+          <img src={card.image} alt="card"/>
+          <h2 children={card.heading} />
+          <Rating value={4} />
+          <p>{card.text}</p>
+          <Button>Add to Cart</Button>
         </div>
-)}
-         </div>
-      )
-     
+      )}
+    </div>
+  )   
 }
 
 export default Card
-
